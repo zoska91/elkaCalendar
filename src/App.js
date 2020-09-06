59e198fb61e5e6be14b02a3ac151d02a5bfc4fdc
Paulina <70859448+Paullina26@users.noreply.github.com>
2020-09-06 21:14:53 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-import GlobalStyle from './styles/GlobalStyle';
-import { theme } from './styles/theme';
-import { routes } from './routes';
+import GlobalStyle from "./styles/GlobalStyle";
+import { theme } from "./styles/theme";
+import { routes } from "./routes";
 
-import { isInitialized } from './components/Firebase/firebase';
-import ReactNotification from 'react-notifications-component';
+import { isInitialized } from "./components/Firebase/firebase";
+import ReactNotification from "react-notifications-component";
 
-import Indicator from './components/Shared/Indicator';
+import Indicator from "./components/Shared/Indicator";
 
-import LoginPage from './pages/LoginPage';
-import InstructorAddLessonPage from './pages/InstructorAddLessonPage';
-import InstructorHomePage from './pages/InstructorHomePage';
-import InstructorListOfStudetnsPage from './pages/InstructorListOfStudetnsPage';
-import HomePage from './pages/HomePage';
-import CreateAccountPage from './pages/CreateAccountPage';
+import LoginPage from "./pages/LoginPage";
+import InstructorAddLessonPage from "./pages/InstructorAddLessonPage";
+import InstructorHomePage from "./pages/InstructorHomePage";
+import InstructorListOfStudetnsPage from "./pages/InstructorListOfStudetnsPage";
+import HomePage from "./pages/HomePage";
+import CreateAccountPage from "./pages/CreateAccountPage";
 
 function App() {
   const [firebaseInitialized, setFirebaseInitialized] = useState(false);
@@ -35,31 +35,15 @@ function App() {
         <Switch>
           <Route exact path={routes.home} component={HomePage} />
           <Route exact path={routes.login} component={LoginPage} />
-          <Route
-            exact
-            path={routes.addLesson}
-            component={InstructorAddLessonPage}
-          />
-          <Route
-            exact
-            path={routes.homeInstructor}
-            component={InstructorHomePage}
-          />
-          <Route
-            exact
-            path={routes.listOfStudents}
-            component={InstructorListOfStudetnsPage}
-          />
-          <Route
-            exact
-            path={routes.createAccountPage}
-            component={CreateAccountPage}
-          />
+          <Route exact path={routes.addLesson} component={InstructorAddLessonPage} />
+
+          <Route exact path={routes.listOfStudents} component={InstructorListOfStudetnsPage} />
+          <Route exact path={routes.createAccountPage} component={CreateAccountPage} />
         </Switch>
       </ThemeProvider>
     </Router>
   ) : (
-    <div id='loader'>
+    <div id="loader">
       <Indicator />
     </div>
   );
